337afd38e034589c1f80be3df7792f8f7e40329d
Quart0 <105156916+Quart0@users.noreply.github.com>
2022-05-22 15:27:01 +0300

    1.0.9
    · added ball physics

--- ping.py
+++ ping.py
@@ -1,4 +1,3 @@
-
 from pygame import *
 from random import randint
 
@@ -8,6 +7,9 @@ display.set_caption("pinghope")
 background = transform.scale(image.load("background.png") , (700,500))
 
 finish = False
+
+speed_x = 3
+speed_y = 3
 
 
 clock = time.Clock()
@@ -29,15 +31,27 @@ class player(GameSprite):
     def update1(self):
         keys_pressed = key.get_pressed()
 
+        if keys_pressed[K_s] and self.rect.x > -100:
+            self.rect.y += self.speed
+    
+        if keys_pressed[K_w] and self.rect.x < 600:
+            self.rect.y -= self.speed
+    def update2(self):
+        keys_pressed = key.get_pressed()
+
         if keys_pressed[K_DOWN] and self.rect.x > 5:
             self.rect.y += self.speed
     
-        if keys_pressed[K_UP] and self.rect.x < 600:
+        if keys_pressed[K_UP] and self.rect.x < 700:
             self.rect.y -= self.speed
+       
 
 
-May1 = player("New Piskel.png",10,10,10,200,200) 
-  
+
+May1 = player("New Piskel.png",50,10,10,30,150) 
+May2 = player("New Piskel 2.png",600,10,10,30,150) 
+ball = player("ballchime.png",300,0,10,30,30) 
+
 
 
 
@@ -49,7 +63,19 @@ while game:
 
     if finish  !=  True:
         window.blit(background,(0,0))
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
         May1.update1()
         May1.reset()
+        May2.update2()
+        May2.reset()
+        ball.reset()
+        if sprite.collide_rect(May1,ball) or sprite.collide_rect(May2,ball):
+            speed_x *= -1
+        if ball.rect.y > 500 or ball.rect.y < 0:
+            speed_y *= -1
+
+
+
     display.update()
     clock.tick(FPS)
